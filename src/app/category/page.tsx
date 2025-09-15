83d7e776bd4db82d372cb33fdffb72ed0ed41b92
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-09-15 06:09:09 +0200

    adding style changes

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -46,56 +46,61 @@ const CategoryPage = () => {
     return (
         <>
             <div className="grow">
-            {user?.favouriteCategory && (
-                <h2 className="text-xl font-semibold text-center text-amber-950 p-3 mb-6">
-                    Current Favourite Category:{" "}
-                    <span className="text-amber-950">{user.favouriteCategory}</span>
-                </h2>
-            )}
-
-            
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-6 px-4 max-w-[350px] sm:max-w-4xl mx-auto w-full majustify-items-center">
-
-                {selectedCategory.map((item: CategoryType, index: number) => (
-                    <div
-                        key={index}
-                        className=" max-w-[800px] flex flex-col items-center bg-gray-700 rounded-2xl p-6 shadow-lg hover:scale-105 transition-transform"
-                    >
-                        {/* Category Name */}
+                {user?.favouriteCategory && (
+                    <div className="mb-8 flex flex-col m-auto mt-8  bg-gray-700 text-white shadow-md rounded-lg p-4 w-full max-w-md text-center">
+                        <p className="text-white font-bold text-lg mb-2">Favourite Category</p>
                         <Link
-                            className="text-white text-lg font-semibold text-center mb-4 hover:underline"
-                            href={`/category/${item.categoryName}`}
+                            className="text-lg font-bold text-blue-200 hover:text-amber-400 transition"
+                            href={`/category/${user.favouriteCategory}`}
                         >
-                            {item.categoryName}
+                            🍴 {user.favouriteCategory}
                         </Link>
-
-                        {/* Category Image */}
-                        <img
-                            className="w-40 h-40 object-cover rounded-lg shadow-md mb-4"
-                            src={item.categoryImage}
-                            alt={item.categoryName}
-                        />
-
-                        {/* Favourite Icon */}
-                        <div className="flex justify-center mt-2">
-                            <img
-                                className="w-10 h-10 sm:w-12 sm:h-12 cursor-pointer hover:scale-110 transition-transform"
-                                onClick={() => favSelected(item.categoryName)}
-                                src={
-                                    user?.favouriteCategory === item.categoryName
-                                        ? "favourite_icon.png"
-                                        : "unfavourite_icon.png"
-                                }
-                                alt={
-                                    user?.favouriteCategory === item.categoryName
-                                        ? `Favourite ${item.categoryName}`
-                                        : `Not Favourite ${item.categoryName}`
-                                }
-                            />
-                        </div>
                     </div>
-                ))}
-            </div>
+                )}
+
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-6 px-4 max-w-[350px] sm:max-w-4xl mx-auto w-full majustify-items-center">
+
+                    {selectedCategory.map((item: CategoryType, index: number) => (
+                        <div
+                            key={index}
+                            className=" max-w-[800px] flex flex-col items-center bg-gray-700 rounded-2xl p-6 shadow-lg hover:scale-105 transition-transform"
+                        >
+                            {/* Category Name */}
+                            <Link
+                                className="text-white text-lg font-semibold text-center mb-4 hover:underline"
+                                href={`/category/${item.categoryName}`}
+                            >
+                                {item.categoryName}
+                            </Link>
+
+                            {/* Category Image */}
+                            <img
+                                className="w-40 h-40 object-cover rounded-lg shadow-md mb-4"
+                                src={item.categoryImage}
+                                alt={item.categoryName}
+                            />
+
+                            {/* Favourite Icon */}
+                            <div className="flex justify-center mt-2">
+                                <img
+                                    className="w-10 h-10 sm:w-12 sm:h-12 cursor-pointer hover:scale-110 transition-transform"
+                                    onClick={() => favSelected(item.categoryName)}
+                                    src={
+                                        user?.favouriteCategory === item.categoryName
+                                            ? "favourite_icon.png"
+                                            : "unfavourite_icon.png"
+                                    }
+                                    alt={
+                                        user?.favouriteCategory === item.categoryName
+                                            ? `Favourite ${item.categoryName}`
+                                            : `Not Favourite ${item.categoryName}`
+                                    }
+                                />
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
 
